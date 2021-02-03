0011475212fe2d5a8cd315ad83450bd0d459840a
Pushkar Bhadur <pushkar.bhadur@onx.ai>
2021-02-03 17:34:10 +0530

    added forms

--- src/modules/forms/reactive.tsx
+++ src/modules/forms/reactive.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import * as React from "react";
+import { useForm } from "react-hook-form";
+import './reactive.css';
 
 export default function ReactiveForm() {
-    return (
-        <div>
-            Reactive form
-        </div>
-    )
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data: any) => alert(JSON.stringify(data));
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input name="firstName" ref={register} placeholder="First name" />
+
+      <input name="lastName" ref={register} placeholder="Last name" />
+
+      <select name="category" ref={register}>
+        <option value="">Select...</option>
+        <option value="A">Category A</option>
+        <option value="B">Category B</option>
+      </select>
+
+      <input type="submit" />
+    </form>
+  );
 }
--- src/modules/forms/template.tsx
+++ src/modules/forms/template.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import * as React from "react";
+import { useForm } from "react-hook-form";
+import './reactive.css';
 
-export default function TemplateForm() {
-    return (
-        <div>
-            Template
-        </div>
-    )
+export default function ReactiveForm() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data: any) => alert(JSON.stringify(data));
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input name="firstName" ref={register} placeholder="First name" />
+
+      <input name="lastName" ref={register} placeholder="Last name" />
+      <input name="email" ref={register} placeholder="Email" />
+      <input name="phone" ref={register} placeholder="phone" />
+
+      <select name="category" ref={register}>
+        <option value="">Select...</option>
+        <option value="A">Category A</option>
+        <option value="B">Category B</option>
+      </select>
+
+      <input type="submit" />
+    </form>
+  );
 }
